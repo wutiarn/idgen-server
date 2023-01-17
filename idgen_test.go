7ab4c8eaf2d9fdbf66628416b56bf39179303142
Dmitry Romanov <me@wtrn.ru>
2023-01-18 00:49:32 +0300

    WIP: idgen.go

--- idgen_test.go
+++ idgen_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"math"
 	"testing"
 )
 
@@ -12,14 +13,36 @@ func TestGenerateId(t *testing.T) {
 		domain:    9,
 	}
 
-	id := generateIdForParams(params)
-	var expected int64 = 449358206980867337
-	if id != expected {
-		t.Errorf("generateIdForParams returned %v, expected %v", id, expected)
-	}
+	var id int64 = 0
 
-	parsedParams := parseIdToParams(id)
-	if parsedParams != params {
-		t.Errorf("parseIdToParams returned %v, expected %v", parsedParams, params)
-	}
+	t.Run("Generate", func(t *testing.T) {
+		id = generateIdForParams(params)
+		var expected int64 = 449358206980867337
+		if id != expected {
+			t.Errorf("generateIdForParams returned %v, expected %v", id, expected)
+		}
+	})
+
+	t.Run("Parse", func(t *testing.T) {
+		parsedParams := parseIdToParams(id)
+		if parsedParams != params {
+			t.Errorf("parseIdToParams returned %v, expected %v", parsedParams, params)
+		}
+	})
+
+	t.Run("Generate timestamp overflow", func(t *testing.T) {
+		modifiedParams := params
+		modifiedParams.timestamp = 34359738371
+		generated := generateIdForParams(modifiedParams)
+		var expected int64 = 805324041
+		if generated != expected {
+			t.Errorf("generateIdForParams returned %v, expected %v", id, expected)
+		}
+
+		parsedParams := parseIdToParams(generated)
+		modifiedParams.timestamp = modifiedParams.timestamp - uint64(math.Pow(2, 35))
+		if parsedParams != modifiedParams {
+			t.Errorf("parseIdToParams returned %v, expected %v", parsedParams, modifiedParams)
+		}
+	})
 }
