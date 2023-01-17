0753b8d969127da205389e80cbf49d9a8982349a
Dmitry Romanov <me@wtrn.ru>
2023-01-18 01:14:41 +0300

    WIP: idgen.go

--- idgen.go
+++ idgen.go
@@ -5,49 +5,51 @@ import (
 	"time"
 )
 
-const epochStart = uint64(1672531200) // 2023-01-01 00:00:00
+const epochStart = 1672531200 // 2023-01-01 00:00:00
 const timestampBits = 35
 const counterBits = 14
 const serverIdBits = 6
 const domainBits = 8
 
-func GenerateId(domain uint8) int64 {
+func GenerateId(domain int8) int64 {
 	params := idParams{
 		timestamp: time.Now(),
 		counter:   1,
 		serverId:  5,
-		domain:    uint64(domain),
+		domain:    domain,
 	}
 	return generateIdForParams(params)
 }
 
 func generateIdForParams(params idParams) int64 {
 	var id int64 = 0
-	timestamp := uint64(params.timestamp.Unix()) - epochStart
+	timestamp := params.timestamp.Unix() - epochStart
 	id = encodePart(id, timestamp, timestampBits)
-	id = encodePart(id, params.counter, counterBits)
-	id = encodePart(id, params.serverId, serverIdBits)
-	id = encodePart(id, params.domain, domainBits)
+	id = encodePart(id, int64(params.counter), counterBits)
+	id = encodePart(id, int64(params.serverId), serverIdBits)
+	id = encodePart(id, int64(params.domain), domainBits)
 	return id
 }
 
 func parseIdToParams(id int64) idParams {
-	result := idParams{}
 
-	result.domain, id = extractPart(id, domainBits)
-	result.serverId, id = extractPart(id, serverIdBits)
-	result.counter, id = extractPart(id, counterBits)
+	domain, id := extractPart(id, domainBits)
+	serverId, id := extractPart(id, serverIdBits)
+	counter, id := extractPart(id, counterBits)
 	timestamp, id := extractPart(id, timestampBits)
 
-	result.timestamp = time.Unix(int64(timestamp+epochStart), 0)
-
-	return result
+	return idParams{
+		timestamp: time.Unix(int64(timestamp+epochStart), 0),
+		counter:   int16(counter),
+		serverId:  int8(serverId),
+		domain:    int8(domain),
+	}
 }
 
-func encodePart(srcId int64, value uint64, bits int) int64 {
-	mask := uint64(math.Pow(2, float64(bits))) - 1
+func encodePart(srcId int64, value int64, bits int) int64 {
+	mask := int64(math.Pow(2, float64(bits))) - 1
 	value = value & mask
-	return srcId<<bits | int64(value)
+	return srcId<<bits | value
 }
 
 func extractPart(id int64, bits int) (extracted uint64, remainingId int64) {
@@ -59,7 +61,7 @@ func extractPart(id int64, bits int) (extracted uint64, remainingId int64) {
 
 type idParams struct {
 	timestamp time.Time
-	counter   uint64
-	serverId  uint64
-	domain    uint64
+	counter   int16
+	serverId  int8
+	domain    int8
 }
