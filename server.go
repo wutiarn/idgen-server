bc2595850c0524cc0c4bcd92ca6cb82d94a2326d
Dmitry Romanov <me@wtrn.ru>
2023-01-18 16:01:20 +0300

    Fix max domain check

--- server.go
+++ server.go
@@ -28,14 +28,14 @@ func handleGenerateRequest(context *gin.Context) {
 	}
 
 	var domain uint8
-	domainStr, domainPassed := context.GetQuery("Domain")
+	domainStr, domainPassed := context.GetQuery("domain")
 	if domainPassed {
 		domainInt, err := strconv.Atoi(domainStr)
 		if err != nil {
 			context.AbortWithError(400, err)
 			return
 		}
-		if domainInt&int(maxDomainValue) != 0 {
+		if domainInt^int(maxDomainValue) != 0 {
 			context.AbortWithError(400, errors.New(fmt.Sprintf("provided Domain exceed maximum value %v", maxCounterValue)))
 			return
 		}
